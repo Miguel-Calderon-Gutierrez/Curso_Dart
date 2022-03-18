3625b0ffcb1ba5204c29daf590ff5485eb0bfb01
Miguel Calderon <65426233+Miguel-Calderon-Gutierrez@users.noreply.github.com>
2022-03-18 14:40:49 -0500

    ejemplos tipos de datos en dart

---
+++ tipos de datos basicos.dart
@@ -0,0 +1,15 @@
+void main(){
+  
+  String nombre = 'Miguel';
+  String apellido = 'Calderon';
+  
+  print('$nombre $apellido');
+  
+  int x = 10;
+  double y = 10.99;
+  
+  print('X vale $x, Y vale $y');
+  
+  var dinamica = 'cadena dinamica';
+  print(dinamica);
+}
